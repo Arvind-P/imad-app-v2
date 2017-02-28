7a70d731dbd64a212495f9d449e41f6230182617
Arvind-P <rvind95@gmail.com>
2017-02-28 15:43:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/ui/articleone.html', function(req, res) {
+app.get('/ui/articleone', function(req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'articleone.html'));
 });
 
