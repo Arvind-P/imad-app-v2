c1efd38e2627cd8343e69a1b443bf29134e504c2
Arvind-P <rvind95@gmail.com>
2017-03-07 17:03:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,5 @@
 console.log('Loaded!');
 
-var changedheading = document.querySelector('h1');
-// var changedheading = document.getElementById('mainheading');
-changedheading.textContent = 'This is a new content that is popped by the main.js file';
-
 alert('Click on the image and it will move gradually');
 //move the image using javascript
 var img = document.getElementById('imgmadi');
@@ -24,6 +20,10 @@ function marginRight()
 
 img.onclick = function()
 {
+    // var changedheading = document.querySelector('h1');
+    var changedheading = document.getElementById('maincontent');
+    changedheading.textContent = 'This is a new content that is popped by the main.js file';
+    
     var interval = setInterval(marginRight,50);        
     //the marginRight function will be called every 50ms and the image is gradually moved
     //img.style.marginLeft = '100px';
