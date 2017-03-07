b3d3033fa3a60ffee09402fbcabcbe38b9fe31e4
Arvind-P <rvind95@gmail.com>
2017-03-07 15:26:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,20 @@
 console.log('Loaded!');
 
+alert('Click on the image and it will move gradually');
 //move the image using javascript
 var img = document.getElementById('imgmadi');
 var marginleft = 0;
 function marginRight()
 {
-    marginleft = marginleft + 3;
-    img.style.marginLeft = marginleft + 'px';
+    if(marginleft < 500)
+    {
+        marginleft = marginleft + 3;
+        img.style.marginLeft = marginleft + 'px';
+    }
+    else
+    {
+        alert('Image movement stopped');
+    }
 }
 
 img.onclick = function()
