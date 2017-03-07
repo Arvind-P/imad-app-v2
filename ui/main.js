2eadbf385e362af5792666427d2dfef48bd4a281
Arvind-P <rvind95@gmail.com>
2017-03-07 15:22:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,12 +5,13 @@ var img = document.getElementById('imgmadi');
 var marginleft = 0;
 function marginRight()
 {
-    marginleft = marginleft + 10;
+    marginleft = marginleft + 3;
     img.style.marginLeft = marginleft + 'px';
 }
 
 img.onclick = function()
 {
-    var interval = setInterval(marginRight,100);
+    var interval = setInterval(marginRight,50);        
+    //the marginRight function will be called every 50ms and the image is gradually moved
     //img.style.marginLeft = '100px';
 }
