580305085eb6d0183825505e9742ed4d856c0a0a
Arvind-P <rvind95@gmail.com>
2017-03-07 17:20:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,12 @@ alert('Click on the image and it will move gradually');
 //move the image using javascript
 var img = document.getElementById('imgmadi');
 var marginleft = 0;
+
+var lastaccessedtime = '';
+
+var lasttime = document.getElementById('lasttime');
+lasttime.textContent = "Last Access Time : " + lastaccessedtime;
+
 function marginRight()
 {
     if(marginleft < 500)
@@ -22,7 +28,7 @@ img.onclick = function()
 {
     // var changedheading = document.querySelector('h1');
     var changedheading = document.getElementById('maincontent');
-    changedheading.textContent = 'This is a new content that is dynamically prompted by the main.js file';
+    changedheading.textContent = 'This is a new content that is dynamically prompted by the main.js file <br />';
     
     var interval = setInterval(marginRight,50);        
     //the marginRight function will be called every 50ms and the image is gradually moved
