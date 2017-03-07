5d6ce897528accabfd7e264a9701abe4d5cb44f1
Arvind-P <rvind95@gmail.com>
2017-03-07 15:19:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,15 @@ console.log('Loaded!');
 
 //move the image using javascript
 var img = document.getElementById('imgmadi');
+var marginleft = 0;
+function marginRight()
+{
+    marginleft = marginleft + 10;
+    img.style.marginLeft = marginleft + 'px';
+}
+
 img.onclick = function()
 {
-    img.style.marginLeft = '100px';
+    var interval = setInterval(marginRight,100);
+    //img.style.marginLeft = '100px';
 }
