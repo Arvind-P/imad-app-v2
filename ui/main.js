26c64b57b43ce917c2f9802f4dd24335cfc096b7
Arvind-P <rvind95@gmail.com>
2017-03-08 15:22:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,28 @@
-var counter = 0;
 var spanelement = document.getElementById('updcounter');
 var buttonelement = document.getElementById('buttonclickme');
 
+// buttonelement.onclick = function() {
+//     //this updates the counter locally. once the index.html is closed and reopened, the counting restarts
+//     counter = counter + 1;
+//     spanelement.innerHTML = counter.toString();
+// };
+
 buttonelement.onclick = function() {
-    counter = counter + 1;
-    spanelement.innerHTML = counter.toString();
+    //create a request to the '/ui/updatebuttonclickcounter' in the server.js file
+    //this increments a counter
+    //get that counter and push it to the spanelement
+    
+    //Make a new httpRequest
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function() {
+        if(request.readystate === XMLHttpRequest.Done) {
+            if(request.status === 200)
+            {
+                //If the request has been completed and is success, then read the counter value returned by the request
+                var counter = request.responseText;
+                spanelement.innerHTML = counter.toString();
+            }
+        }
+    };
 };
